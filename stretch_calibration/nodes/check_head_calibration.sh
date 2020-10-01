e428305d5adeec00001593ecca2139f8d3098d1b
Charlie Kemp <31106448+hello-ck@users.noreply.github.com>
2020-10-01 18:10:52 -0400

    efficiently check calibration
    Code to check the existing calibration. It does the following:
    
    + Collects a reduced number of new observations with which to check the current calibration.
    
    + Visualizes and computes the fit of the current calibration to the new observations.
    
    Future work:
    
    + Improve the reduced number of observations to increase efficiency.
    
    + Check the numeric results of the fit and provide a warning if the quality is low.

---
+++ stretch_calibration/nodes/check_head_calibration.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "-----------------------------------------------------------"
+echo "Find the most recent optimization results file."
+
+FILENAME=$HELLO_FLEET_PATH/$HELLO_FLEET_ID"/calibration_ros/head_calibration_result_"
+
+for file in `ls $FILENAME*.yaml | sort -r`; do
+    MOSTRECENT_OPTIMIZATION_FILE=$file
+    break 1
+done
+
+echo "Found $MOSTRECENT_OPTIMIZATION_FILE"
+
+echo "-----------------------------------------------------------"
+echo "Find the most recent controller calibration file."
+
+FILENAME=$HELLO_FLEET_PATH/$HELLO_FLEET_ID"/calibration_ros/controller_calibration_head_"
+
+for file in `ls $FILENAME*.yaml | sort -r`; do
+    MOSTRECENT_CONTROLLER_FILE=$file
+    break 1
+done
+
+echo "Found $MOSTRECENT_CONTROLLER_FILE"
+
+echo "-----------------------------------------------------------"
+echo "Find the most recent calibrated URDF file."
+
+FILENAME=$HELLO_FLEET_PATH/$HELLO_FLEET_ID"/calibration_ros/head_calibrated_"
+
+for file in `ls $FILENAME*.urdf | sort -r`; do
+    MOSTRECENT_URDF=$file
+    break 1
+done
+
+echo "Found $MOSTRECENT_URDF"
+
+echo "-----------------------------------------------------------"
+
+echo "Launch the visualization using these three files."
+echo "roslaunch stretch_calibration visualize_head_calibration.launch optimization_result_yaml_file:=$MOSTRECENT_OPTIMIZATION_FILE calibrated_controller_yaml_file:=$MOSTRECENT_CONTROLLER_FILE calibrated_urdf_file:=$MOSTRECENT_URDF" 
+
+roslaunch stretch_calibration check_head_calibration.launch optimization_result_yaml_file:=$MOSTRECENT_OPTIMIZATION_FILE calibrated_controller_yaml_file:=$MOSTRECENT_CONTROLLER_FILE calibrated_urdf_file:=$MOSTRECENT_URDF
