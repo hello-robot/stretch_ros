07f53be79f48fa441146d880f105fd79c146d743
hello-binit <64861565+hello-binit@users.noreply.github.com>
2021-07-31 02:21:21 -0400

    Launch simulated robot in stow position

--- stretch_gazebo/scripts/publish_ground_truth_odom.py
+++ stretch_gazebo/scripts/publish_ground_truth_odom.py
@@ -3,14 +3,17 @@
 from gazebo_msgs.srv import GetModelState, GetModelStateRequest, GetWorldProperties
 from nav_msgs.msg import Odometry
 from std_msgs.msg import Header
+from std_srvs.srv import Empty
 import rospy
+import time
 
 rospy.init_node('ground_truth_odometry_publisher')
 odom_pub=rospy.Publisher('ground_truth', Odometry, queue_size=10)
 
-rospy.wait_for_service ('/gazebo/get_model_state')
+rospy.wait_for_service('/gazebo/get_model_state')
 get_model_srv = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 get_world_properties = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties)
+unpause_physics = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
 
 odom=Odometry()
 header = Header()
@@ -19,6 +22,12 @@ model = GetModelStateRequest()
 model.model_name='robot'
 models = []
 r = rospy.Rate(20)
+
+pause_timeout = time.time() + 4.0
+while time.time() < pause_timeout:
+    rospy.logwarn("Waiting %.2f seconds to unpause physics", pause_timeout - time.time())
+    time.sleep(1.0)
+unpause_physics()
 
 while not rospy.is_shutdown():
     if model.model_name not in models:
