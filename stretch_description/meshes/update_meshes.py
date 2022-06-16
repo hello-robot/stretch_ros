0c4a1f5b0753378ffb6730cfa77e55b5b51fd2fa
Aaron Edsinger <aedsinger@hello-robot.com>
2022-06-16 10:35:23 -0700

    fix update_meshes

--- stretch_description/meshes/update_meshes.py
+++ stretch_description/meshes/update_meshes.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 import os
 import stretch_body.hello_utils as hu
-try:
-    import stretch_body.robot_params
-    batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
-    cmd='cp ~/catkin_ws/src/stretch_ros/stretch_description/meshes/'+batch_name+'/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes'
-    print("Copying in mesh files from batch:"+batch_name)
-    print(cmd)
-    os.system(cmd)
-except hu.ParameterMigrationRequired(Exception):
-    pass
+import stretch_body.robot_params
+batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
+cmd='cp ~/catkin_ws/src/stretch_ros/stretch_description/meshes/'+batch_name+'/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes'
+print("Copying in mesh files from batch:"+batch_name)
+print(cmd)
+os.system(cmd)
