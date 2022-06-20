01b639a9380f1dde3dcc0d80a47350ce4e1471a1
Aaron Edsinger <aedsinger@hello-robot.com>
2022-06-20 11:53:29 -0700

    allow update_meshes.py to work with both param formats

--- stretch_description/meshes/update_meshes.py
+++ stretch_description/meshes/update_meshes.py
@@ -2,7 +2,10 @@
 import os
 import stretch_body.hello_utils as hu
 import stretch_body.robot_params
-batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
+if os.path.exists(hu.get_fleet_directory()+'stretch_configuration_params.yaml'):
+    batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
+else:
+    batch_name = hu.read_fleet_yaml('stretch_re1_factory_params.yaml')['robot']['batch_name'].lower()
 cmd='cp ~/catkin_ws/src/stretch_ros/stretch_description/meshes/'+batch_name+'/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes'
 print("Copying in mesh files from batch:"+batch_name)
 print(cmd)
