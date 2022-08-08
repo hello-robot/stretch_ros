938d1cff5f129cf0c09eaddfa768201802536aaa
Binit Shah <bshah@hello-robot.com>
2022-08-08 11:39:46 -0700

    Fix aliasing bug in update_meshes.py

--- stretch_description/meshes/update_meshes.py
+++ stretch_description/meshes/update_meshes.py
@@ -2,7 +2,8 @@
 import os
 from os.path import exists
 import stretch_body.hello_utils as hu
-if exists(hello_utils.get_fleet_directory()+'stretch_user_params.yaml'):
+
+if exists(hu.get_fleet_directory()+'stretch_user_params.yaml'):
     batch_name = hu.read_fleet_yaml('stretch_user_params.yaml')['robot']['batch_name'].lower()
 else:
     batch_name = hu.read_fleet_yaml('stretch_re1_factory_params.yaml')['robot']['batch_name'].lower()
