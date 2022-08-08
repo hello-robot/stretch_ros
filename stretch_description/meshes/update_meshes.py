9e24402eb4d888c6255b29ad1dedeedea5c8a1f8
Binit Shah <bshah@hello-robot.com>
2022-08-08 12:50:20 -0700

    Fix bug in update_meshes.py

--- stretch_description/meshes/update_meshes.py
+++ stretch_description/meshes/update_meshes.py
@@ -3,8 +3,8 @@ import os
 from os.path import exists
 import stretch_body.hello_utils as hu
 
-if exists(hu.get_fleet_directory()+'stretch_user_params.yaml'):
-    batch_name = hu.read_fleet_yaml('stretch_user_params.yaml')['robot']['batch_name'].lower()
+if exists(hu.get_fleet_directory()+'stretch_configuration_params.yaml'):
+    batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
 else:
     batch_name = hu.read_fleet_yaml('stretch_re1_factory_params.yaml')['robot']['batch_name'].lower()
 cmd='cp ~/catkin_ws/src/stretch_ros/stretch_description/meshes/'+batch_name+'/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes'
