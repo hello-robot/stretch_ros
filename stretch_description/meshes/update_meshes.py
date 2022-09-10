145096496c3ff7a56c951228cf60e36e68bcb9f5
Binit Shah <bshah@hello-robot.com>
2022-09-09 17:28:36 -0700

    update_meshes exits when copying command fails

--- stretch_description/meshes/update_meshes.py
+++ stretch_description/meshes/update_meshes.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import os
 from os.path import exists
 import stretch_body.hello_utils as hu
+import sys
 
 if exists(hu.get_fleet_directory() + 'stretch_configuration_params.yaml'):
     batch_name = hu.read_fleet_yaml('stretch_configuration_params.yaml')['robot']['batch_name'].lower()
@@ -9,6 +11,8 @@ else:
     batch_name = hu.read_fleet_yaml('stretch_re1_factory_params.yaml')['robot']['batch_name'].lower()
 
 cmd='cp ~/catkin_ws/src/stretch_ros/stretch_description/meshes/'+batch_name+'/*.STL ~/catkin_ws/src/stretch_ros/stretch_description/meshes'
-print("Copying in mesh files from batch:"+batch_name)
+print("Copying in mesh files from batch: "+batch_name)
 print(cmd)
-os.system(cmd)
+if os.system(cmd) != 0:
+    print('update_meshes.py ERROR: failed to execute copying command', file=sys.stderr)
+    sys.exit(1)
