a187de81940c3aa5a367fe45ca92800c3716fb11
hello-chintan <cdesai@hello-robot.com>
2023-02-20 19:24:44 -0500

    Move base action initial commit

---
+++ stretch_navigation/nodes/move_base_pose.py
@@ -0,0 +1,8 @@
+import move_base
+from move_base_msgs.msg import MoveBaseActionGoal
+from move_base_msgs.msg import MoveBaseActionFeedback
+from move_base_msgs.msg import MoveBaseActionResult
+from geometry_msgs.msg import PoseStamped
+
+
+
